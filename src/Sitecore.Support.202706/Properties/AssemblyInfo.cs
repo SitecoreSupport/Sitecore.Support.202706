ab1cf3b802c99471627abebbda0f7d5fd5ab0676
Pavel <piv@sitecore.net>
2018-01-19 13:08:15 +0200

    #202706: Initial commit

---
+++ src/Sitecore.Support.202706/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.202706")]
+[assembly: AssemblyProduct("Sitecore.Support.202706")]
+[assembly: ComVisible(false)]
